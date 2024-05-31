3a31ecfbccb8f2f82ce493cad538ca69c30e988e
Fidan Mammadova <imfidanmamedova@gmail.com>
2024-05-31 12:02:03 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './layout/header'
+
+const App = () => {
+  return (
+    <div className='bg-black'>
+      <Header/>
+    </div>
+  )
+}
+
+export default App
